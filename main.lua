acbf17e126911b9051f2f5c7be989f1080842ed7
stormskmonkey <113997173+stormskmonkey@users.noreply.github.com>
2025-09-08 17:28:12 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-if getgenv().jinkX["Fish It"]["Enabled"] then
+if getgenv().jinkX and getgenv().jinkX["Fish It"] and getgenv().jinkX["Fish It"]["Enabled"] then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/stormskmonkey/FishIt/refs/heads/main/FullOptionLoader.lua"))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/stormskmonkey/FishIt/refs/heads/main/NLoader.lua"))()
